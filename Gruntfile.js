5e2787f44d98ae74be90472d688a94b5e76d4967
amanda <amandayoung686@gmail.com>
2016-07-31 17:37:35 -0500

    getting grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,48 @@
 module.exports = function(grunt) {
 
-    grunt.registerTask( 'default', ['watch'] );
+    grunt.registerTask( 'default', ['open', 'watch']);
+    grunt.registerTask('dev' ['open:dev', 'watch']);
+    grunt.registerTask('build'['build','server', 'open:build', 'watch:build']);
 
     grunt.initConfig({
-
-        watch: {
-                files: [
-                    
-                    './pyPhotography/**/*.js',
-                    './pyPhotography/**/*.css',
-                    './pyPhotography/**/*.html',
-                    './pyPhotography/**/*.html',
-                    'Gruntfile.js'
-                    
-                
-                    
-                ],
-    
-                options: {
-                    spawn: false
-                    }
+        open : {
+            dev : {
+                path:'http://127.0.0.1:8888/src',
+                app:'Safari'
+        
+            },
+            build : {
+                path: 'http://google.com/',
+                app: 'Firefox'
+            },
+            file : {
+                path : '/etc/hosts'
+            },
+            custom : {
+                path : function () {
+                    return grunt.option ('path');
                 }
-            }   
-        ),
-
-
+            }
+        }
+    })
+//
+//        watch: {
+//                files: [
+//                    
+//                    './pyPhotography/**/*.js',
+//                    './pyPhotography/**/*.css',
+//                    './pyPhotography/**/*.html',
+//                    './pyPhotography/**/*.html',
+//                    'Gruntfile.js'
+//                    
+//                ],
+//    
+//                options: {
+//                    spawn: false
+//                    }
+//                }   
+    
+    grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-contrib-watch');
     
             
